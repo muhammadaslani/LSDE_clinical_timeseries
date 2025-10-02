a436ff11603300e2db4a168d3127e6ecb49a25b3
muhammad aslani <muhammad.aslani.81@gmail.com>
2025-10-02 12:04:46 +0200

    interploation changed from BSpline to linear in dynamics.jl

--- src/core/dynamics.jl
+++ src/core/dynamics.jl
@@ -57,8 +57,8 @@ returns:
 function (de::SDE)(x::AbstractArray, u::Union{Nothing, AbstractArray}, c::AbstractArray, ts::AbstractArray, p::ComponentVector, st::NamedTuple)
     #TBD to fix the interpolation
 
-    u_cont1(t) =  interp!(ts, u, t, Val(:BSpline))
-    u_cont2(t) =  interp!(ts, u, t, Val(:BSpline))
+    u_cont1(t) =  interp!(ts, u, t, Val(:linear))
+    u_cont2(t) =  interp!(ts, u, t, Val(:linear))
     c_cont(t) =  interp!(ts, c, t, Val(:linear))
 
     function μ_augmented(x, p, t)
@@ -168,8 +168,8 @@ Arguments :
 """
 function sample_augmented(de::SDE, init_map, solver, px₀, u, c, ts, p, st, n_samples, dev; kwargs...)
     tspan = (ts[1], ts[end])
-    u_cont(t) = interp!(ts, u, t, Val(:binary))
-    c_cont(t) = interp!(ts, c, t, Val(:BSpline))
+    u_cont(t) = interp!(ts, u, t, Val(:linear))
+    c_cont(t) = interp!(ts, c, t, Val(:linear))
 
     x₀ = init_map(sample_rp(px₀), p.init_map, st.init_map)[1]
 
